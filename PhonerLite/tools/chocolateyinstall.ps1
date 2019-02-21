5d39d79be1c04fd8d38e1a0e1d2b90548285cef6
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2019-02-21 13:28:23 +0100

    cleanup

---
+++ PhonerLite/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.phoner.de/PhonerLiteSetup.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'PhonerLite*'
+
+  checksum      = '7561002D81A52D82D979C435D78891A7902C1C33988ABF0CC07531E4E21610AC'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
