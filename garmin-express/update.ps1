7a8919e5d27ac7c64305959c7c01d9b05a19ddfb
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2019-03-16 15:40:05 +0100

    cleanup

--- garmin-express/update.ps1
+++ garmin-express/update.ps1
@@ -7,10 +7,7 @@ $forumsticky = "https://forums.garmin.com/search?searchJSON=%7B%22keywords%22%3A
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            # "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            # "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            # "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
