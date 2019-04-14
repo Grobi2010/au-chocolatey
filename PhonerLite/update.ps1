46b99e66bdf11bab6fbbf8f070d8a726d6a041c0
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2019-04-14 20:52:08 +0200

    remove url

--- PhonerLite/update.ps1
+++ PhonerLite/update.ps1
@@ -6,8 +6,7 @@ $url32 = 'https://www.phoner.de/PhonerLiteSetup.exe'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+           "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
