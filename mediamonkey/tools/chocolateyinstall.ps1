5ef71e0cc79d2170605009c5a0e35459bb91518a
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2019-06-28 13:02:53 +0200

    MediaMonkey added

---
+++ mediamonkey/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'mediamonkey'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.mediamonkey.com/sw/MediaMonkey_4.1.23.1881.exe'
+$url64      = ''
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+  softwareName  = 'mediamonkey*'
+  checksum      = 'deb68a7c93de27671bc668e25b7ed687b088ffd5d437653e6512eff2cf9e6fba'
+  checksumType  = 'sha512'
+  checksum64    = ''
+  checksumType64= 'sha512'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ mediamonkey/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$url = 'https://www.mediamonkey.com/MediaMonkey_Setup.exe'
+$forumsticky = "https://www.mediamonkey.com/download"
+
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $forumsticky
+    
+    $content = $download_page.tostring() -split "[`r`n]" | select-string "Download MediaMonkey for Windows "
+    [regex]$regex = '[0-9][0-9]?.[0-9][0-9]?.[0-9][0-9]?'
+    $version = $regex.Matches($content) |ForEach-Object {$_.Value}
+    
+    @{
+        URL32   = $url
+        Version = $version
+    }
+}
+
+
+update
