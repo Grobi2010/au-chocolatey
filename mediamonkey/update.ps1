09436e6b4dac6456ed4eab16933cda0a2a0eedf4
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2019-10-18 15:36:47 +0200

    Update update.ps1
    MM download correct file

--- mediamonkey/update.ps1
+++ mediamonkey/update.ps1
@@ -1,8 +1,8 @@
 import-module au
 
 $NoCheckChocoVersion = 'true'
-$url = 'https://www.mediamonkey.com/MediaMonkey_Setup.exe'
-$forumsticky = "https://www.mediamonkey.com/download"
+$url = 'https://www.mediamonkey.com/sw/MediaMonkey_'
+$forumsticky = "https://www.mediamonkey.com/support/index.php?/Knowledgebase/List/Index/19/downloads"
 
 
 function global:au_SearchReplace {
@@ -16,12 +16,12 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $forumsticky
     
-    $content = $download_page.tostring() -split "[`r`n]" | select-string "Download MediaMonkey for Windows "
-    [regex]$regex = '[0-9][0-9]?.[0-9][0-9]?.[0-9][0-9]?'
+    $content = $download_page.tostring() -split "[`r`n]" | select-string "MediaMonkey for Windows " | Select-Object -First 1
+    [regex]$regex = '[0-9][0-9]?[.][0-9][0-9]?[.][0-9][0-9]?[.][0-9][0-9][0-9][0-9]?'
     $version = $regex.Matches($content) |ForEach-Object {$_.Value}
     
     @{
-        URL32   = $url
+        URL32   = $url + $version + ".exe"
         Version = $version
     }
 }
