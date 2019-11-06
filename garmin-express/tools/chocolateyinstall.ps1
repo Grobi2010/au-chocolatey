b926fc67a08bfad57e407aa2a2446f8f18cf70c2
Chocolatey <chocolatey@realdimensions.net>
2019-11-06 16:06:17 +0000

    AU: 1 updated - garmin-express [skip ci]

--- garmin-express/tools/chocolateyinstall.ps1
+++ garmin-express/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   fileType      = "EXE"
   url			= "https://download.garmin.com/omt/express/GarminExpress.exe"
 
-  checksum      = 'd2ce71cec04aed15fce1fbb78c20673e7dc8244beceb224e420415f75fde3c5e'
+  checksum      = 'f3d898c0bfd8d35036ac756ee5bb36d25af6493d684c61989125205259c55013'
 
   checksumType  = 'sha256'
   silentArgs    = '/s /a /s /v"/qb"'
