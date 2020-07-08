19fec3d2c1ef24c438a024c814dbd6f3d84ddaaa
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2020-07-08 10:29:10 +0200

    Changed Silent Args

--- garmin-express/tools/chocolateyinstall.ps1
+++ garmin-express/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
   fileType      = "EXE"
   url			= "https://download.garmin.com/omt/express/GarminExpress.exe"
 
-  checksum      = '71BD934E249CA1104AD211BDF7EF28A39C0809EC8BE77D313FA45E6D47B84824'
+  checksum      = '0E4A2C4FCB095592169D665A4185D50136512692F71C6D0E33EC6102088F8ABB'
 
   checksumType  = 'sha256'
-  silentArgs    = '/s /a /v'
+  silentArgs    = '/quiet /norestart'
 }
   
 Install-ChocolateyPackage @packageArgs
