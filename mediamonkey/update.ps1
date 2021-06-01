de8269cf8dd0471a642fea56da344f402cb1e02a
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2021-06-01 12:17:11 +0200

    MediaMonkey5

--- mediamonkey/update.ps1
+++ mediamonkey/update.ps1
@@ -1,8 +1,8 @@
 import-module au
 
 $NoCheckChocoVersion = 'true'
-$url = 'https://www.mediamonkey.com/sw/MediaMonkey_'
-$forumsticky = "https://www.mediamonkey.com/support/index.php?/Knowledgebase/List/Index/19/downloads"
+$url = 'https://www.mediamonkey.com/MediaMonkey-5_Setup'
+$forumsticky = "https://www.mediamonkey.com/forum/viewtopic.php?f=3&t=8811"
 
 
 function global:au_SearchReplace {
@@ -16,12 +16,13 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $forumsticky
     
-    $content = $download_page.tostring() -split "[`r`n]" | select-string "MediaMonkey 4." | Select-Object -First 1
+    $content = $download_page.tostring() -split "[`r`n]" | select-string "MediaMonkey: 5." | Select-Object -First 1
     [regex]$regex = '[0-9][0-9]?[.][0-9][0-9]?[.][0-9][0-9]?[.][0-9][0-9][0-9][0-9]?'
-    $version = $regex.Matches($content) |ForEach-Object {$_.Value}
-    
+    $versiontemp = $regex.Matches($content) |ForEach-Object {$_.Value}
+    $version = $versiontemp[0]
+	
     @{
-        URL32   = $url + $version + ".exe"
+        URL32   = $url + ".exe"
         Version = $version
     }
 }
