d0681e863c828b89f87d2c91d53bebebbcbe6560
Alex Lopez <info@ithinkvirtual.com>
2022-05-24 10:26:46 -0400

    Update chocolateyinstall.ps1

--- garmin-express/tools/chocolateyinstall.ps1
+++ garmin-express/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   packageName   = "garmin-express"
   fileType      = "EXE"
-  url			= "https://download.garmin.com/omt/express/GarminExpress.exe"
+  url           = "https://download.garmin.com/omt/express/GarminExpress.exe"
 
   checksum      = '8FD9C1F04002F81E2C1119B854232257A3700BAF12A3689C59F1CCDCFA2F3D6A'
 
