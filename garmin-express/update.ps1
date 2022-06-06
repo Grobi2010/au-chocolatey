d24393fb85075ac94cea755637caefff884623b3
Grobi2010 <39103661+Grobi2010@users.noreply.github.com>
2022-06-06 21:34:02 +0200

    Back to Chip.de

--- garmin-express/update.ps1
+++ garmin-express/update.ps1
@@ -1,9 +1,9 @@
 import-module au
 
 $url = 'https://download.garmin.com/omt/express/GarminExpress.exe'
-$forumsticky = "https://support.garmin.com/en-US/?productID=168768&tab=software"
+# $forumsticky = "https://support.garmin.com/en-US/?productID=168768&tab=software"
 #  $forumsticky = "https://www.computerbild.de/download/Garmin-Express-15153001.html"
-#  $forumsticky = "https://www.chip.de/downloads/Garmin-Express_88198672.html"
+$forumsticky = "https://www.chip.de/downloads/Garmin-Express_88198672.html"
 
 function global:au_SearchReplace {
    @{
@@ -16,8 +16,8 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $forumsticky
     
-    $content = $download_page.tostring() -split "[`r`n]" | select-string "Garmin Express for Windows: "
-	# $content = $download_page.tostring() -split "[`r`n]" | select-string "softwareVersion"
+    # $content = $download_page.tostring() -split "[`r`n]" | select-string "Garmin Express for Windows: "
+	$content = $download_page.tostring() -split "[`r`n]" | select-string "softwareVersion"
     [regex]$regex = '[0-9]\.[0-9][0-9]?\.?[0-9]?[0-9]'
     $version = $regex.Match($content) |ForEach-Object {$_.Value}
     
